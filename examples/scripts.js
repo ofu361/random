3f75f3c80cf566a0608d6ced97fe7e4680adb9ec
ofu361 <student.x.sajad@gmail.com>
2023-09-30 12:44:08 +0200

    Dashborad Script

---
+++ examples/scripts.js
@@ -0,0 +1,11 @@
+function callApi(meta, data){
+    console.log(data)
+    return "Kann gut passieren " + meta;
+}
+
+
+function aviaCars(){
+    let menge=0;
+
+    
+}
--- index.php
+++ index.php
@@ -1,11 +1,35 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<h2>Die akkreditierte Dienstleistungen, die anbieten.</h2>
+
+	<table>
+		<tr>
+			<td>Laser Geräte</td>
+			<td>Haushaltsgeräte</td>
+		</tr>
+		<tr>
+			<td>E-Autos</td>
+			<td>dHaushaltsgeräte</td>
+		</tr>
+	</table>
+	<!-- <a target="_blank" href="assets/images/faheem.png">
+		<img src="assets/images/faheem.png" alt="">
+	</a>
+	<a target="_blank" href="assets/images/sportman.png">
+		<img src="assets/images/sportman.png" alt="">
+	</a>
+	<a target="_blank" href="assets/images/coder.png">
+		<img src="assets/images/coder.png" alt="">
+	</a>
+	<a target="_blank" href="assets/images/chemist.png">
+		<img src="assets/images/chemist.png" alt="">
+	</a> -->
+</body>
+</html>
